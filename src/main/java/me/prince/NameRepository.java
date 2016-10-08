678e08713a4ccdf7f2ead66ebce1e4c0ecfb946c
yhjhoo <yhjhoo@163.com>
2016-10-08 10:12:29 +0800

    spring boot swagger demo

---
+++ src/main/java/me/prince/NameRepository.java
@@ -0,0 +1,20 @@
+package me.prince;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NameRepository {
+	@RequestMapping(value="/person", method=RequestMethod.GET)
+	public List<String> index(){
+		List<String> list = new ArrayList<>();
+		list.add("1");
+		list.add("2");
+		
+		return list;
+	}
+}
---
+++ src/main/java/me/prince/SwaggerDemoApplication.java
@@ -0,0 +1,14 @@
+package me.prince;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("me.prince")
+public class SwaggerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerDemoApplication.class, args);
+	}
+}
